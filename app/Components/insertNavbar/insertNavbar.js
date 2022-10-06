57f0280bcf7077d4b176ebdb4be96d3aec35db7e
Iker-Romero <ikersoftdev@gmail.com>
2022-10-06 21:06:51 +0200

    Navbar added

---
+++ app/Components/insertNavbar/insertNavbar.js
@@ -0,0 +1,11 @@
+export const insertNavbar = () => {
+    document.body.innerHTML += `
+    <header>
+      <nav>
+        <ul>
+          <li>ILMA Gallery</li>
+        </ul>
+      </nav>
+    </header>
+    `
+}
--- app/main.js
+++ app/main.js
@@ -1,7 +1,9 @@
 import "./style.css";
 import { insertImages } from "./Components/insertImages/insertImages";
+import { insertNavbar } from "./Components/insertNavbar/insertNavbar";
 
 const triggerComponents = () => {
+  insertNavbar();
   const paintingsArray = insertImages("http://localhost:3000/paintings");
   console.log(paintingsArray);
 };
