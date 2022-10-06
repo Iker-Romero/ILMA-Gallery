f2dd7cabe5267e11b2cf4dc79fdc1135de651c93
Iker-Romero <ikersoftdev@gmail.com>
2022-10-06 19:52:33 +0200

    renaming and comments removed

--- app/Components/insertImages/insertImages.js
+++ app/Components/insertImages/insertImages.js
@@ -1,15 +1,21 @@
-// import { getImages } from "../getImages/getImages";
+export const insertImages = async (url) => {
+  try {
+    const rawImages = await fetch(url);
+    const jsonImages = await rawImages.json();
+    const div = document.createElement('div');
+    for (const painting of jsonImages) {
+      console.log(painting["image"])
+        div.innerHTML +=  `
+        <figure>
+            <img src="${painting["image"]}" alt="">
+            <figcaption>${painting["title"]}</figcaption>
+            <p>El autor es: ${painting["author"]}</p>
+        </figure>
+        `
+    }
+    document.body.appendChild(div);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-// export const insertImages = (array) => {
-//     const div = document.createElement('div');
-//     for (const painting of array) {
-//         div.innerHTML +=  `
-//         <figure>
-//             <img src="${painting["image"]}" alt="">
-//             <figcaption>${painting["title"]}</figcaption>
-//             <p>El autor es: ${painting["author"]}</p>
-//         </figure>
-//         `
-//     }
-//     document.body.appendChild(div);
-// }
--- app/main.js
+++ app/main.js
@@ -1,9 +1,8 @@
 import "./style.css";
-import { getImages } from "./Components/getImages/getImages";
-// import { insertImages } from "./Components/insertImages/insertImages";
+import { insertImages } from "./Components/insertImages/insertImages";
 
 const triggerComponents = () => {
-  const paintingsArray = getImages("http://localhost:3000/paintings");
+  const paintingsArray = insertImages("http://localhost:3000/paintings");
   console.log(paintingsArray);
 };
 
